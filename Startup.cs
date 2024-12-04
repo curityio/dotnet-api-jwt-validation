5c560e19faedbe74f61487b47cd85ab0b0be3849
Jonas Iggbom <jonas.iggbom@curity.io>
2024-12-04 14:40:31 -0800

    Updated app config. Updated aud validation. Added JWT signature validation

--- Startup.cs
+++ Startup.cs
@@ -1,17 +1,16 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Net.Http;
+using System.Security.Cryptography;
 
 namespace weather
 {
@@ -27,19 +26,32 @@ namespace weather
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string validIssuer = Configuration.GetSection("Authorization:Issuer").Get<string>();
+            string[] validAudiences = Configuration.GetSection("Authorization:Audience").Get<string[]>();
+            string jwksUri = Configuration.GetSection("Authorization:JWKS").Get<string>();
+            string keyId = Configuration.GetSection("Authorization:Kid").Get<string>();
+            string alg = Configuration.GetSection("Authorization:Alg").Get<string>();
+            
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
+                .AddJwtBearer(async options =>
                 {
-                    options.Authority = Configuration["Authorization:Issuer"];
-                    options.Audience = Configuration["Authorization:Audience"];
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = false,
+                        ValidAudiences = validAudiences,
+                        ValidIssuer = validIssuer,
+                        IssuerSigningKey = await GetIssuerSigningKeyAsync(jwksUri, keyId, alg)
+                    };
                 });
-
+                
             services.AddAuthorization(options =>
             {
                 options.AddPolicy("lowRisk", policy =>
                     policy.RequireAssertion(context =>
                         context.User.HasClaim(claim =>
-                            claim.Type == "risk" && Int32.Parse(claim.Value) < 50
+                            claim.Type == "risk" && int.Parse(claim.Value) < 50
                         )
                     )
                 );
@@ -53,6 +65,32 @@ namespace weather
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "weather", Version = "v1" });
             });
+        }
+
+        static private async Task<RsaSecurityKey> GetIssuerSigningKeyAsync(string jwksUri, string keyId, string alg)
+        {
+            using var httpClient = new HttpClient();
+            
+            // Fetch JWKS JSON from URI
+            var response = await httpClient.GetStringAsync(jwksUri);            
+            JsonWebKeySet jwks = new(response);
+            
+            // Find the key with the specified Key ID (kid) and algorithm (alg)
+            var key = jwks.Keys.Single(k => k.Kid == keyId && k.Alg == alg) ?? throw new Exception($"Key with ID '{keyId}' not found.");
+
+            // Extract parameters from the JWK
+            var n = Base64UrlEncoder.DecodeBytes(key.N.ToString());
+            var e = Base64UrlEncoder.DecodeBytes(key.E.ToString());
+
+            // Create an RSA security key
+            var rsa = RSA.Create();
+            rsa.ImportParameters(new RSAParameters
+            {
+                Modulus = n,
+                Exponent = e
+            });
+
+            return new RsaSecurityKey(rsa) { KeyId = keyId };
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
